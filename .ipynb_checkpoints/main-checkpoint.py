5cc4f1cf61a6453ad3da25b55c8bc28646d617bb
yamada <t.y.corp05@gmail.com>
2024-01-23 16:52:31 +0900

    edit main.py

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -2,16 +2,20 @@ import json
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
-file = open('info.json','r')
-info = json.load(file)
+# file = open('info.json','r')
+# info = json.load(file)
 
-CHNNNEL_ACCESS_TOKEN = info['CHNNNEL_ACCESS_TOKEN']
-line_bot_api = LineBotApi(CHNNNEL_ACCESS_TOKEN)
+with open('info.json', 'r') as file:
+    info = json.load(file)
+
+
+CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID = info['USER_ID']
     messages = TextSendMessage(text='ますみさん！\n おはようございます！')
-    line_bot_api.push_message(USER_ID, messages = messages)
+    line_bot_api.push_message(USER_ID, messages)
 
 if __name__=="__main__":
     main()
